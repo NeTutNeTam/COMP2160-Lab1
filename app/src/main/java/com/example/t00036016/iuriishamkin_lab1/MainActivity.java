654726c7de2e8de554cf49f1e351a94ea46e4f0d
NeTutNeTam <nobrain.nopain.mtb@gmail.com>
2015-09-25 14:32:20 -0700

    Final version of the app

--- app/src/main/java/com/example/t00036016/iuriishamkin_lab1/MainActivity.java
+++ app/src/main/java/com/example/t00036016/iuriishamkin_lab1/MainActivity.java
@@ -1,17 +1,109 @@
 package com.example.t00036016.iuriishamkin_lab1;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText kml,mpg;     //declaring fields to enter data
+    String result;        //result of conversion
+    Double conv_value;    //entered data
+    Button buttonConvert; //conversion button
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonConvert = (Button)findViewById(R.id.button01); //initializing button
+        kml = (EditText)findViewById(R.id.editText01); //initializing first input field
+        mpg = (EditText)findViewById(R.id.editText02); //initializing second input field
+
+        kml.addTextChangedListener(new TextWatcher() {  //assigning listener to the first input so
+                                                        //user will be aware of exceeded input length
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length()>11)
+                    kml.setError("Maximum number exceeded");
+            }
+        });
+
+        mpg.addTextChangedListener(new TextWatcher() {  //assigning listener to the second input so
+                                                        //user will be aware of exceeded input length
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length()>11)
+                    mpg.setError("Maximum number exceeded");
+            }
+        });
+
+
+        buttonConvert.setOnClickListener(new View.OnClickListener() { //creating listener for the convert button
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(kml.getText().toString()) && TextUtils.isEmpty(mpg.getText().toString())) { //checking first situation
+                                                                                                                  //when both inputs are empty
+                    kml.setError("One of these two items must have a value!");
+                    Toast.makeText(getApplicationContext(), "Please Enter a Value", Toast.LENGTH_SHORT).show();
+                }
+                else if (TextUtils.isEmpty(kml.getText().toString()) && !TextUtils.isEmpty(mpg.getText().toString())) { //checking when second
+                                                                                                                        //input isn't empty but first
+
+                    try {
+                        conv_value = Double.parseDouble(mpg.getText().toString());
+                    } catch (NumberFormatException e) {
+                        conv_value = 0.0;
+                    }
+                    conv_value *= 0.42;
+                    result = String.format("%.2f",conv_value);
+                    kml.setText(result);
+                    Toast.makeText(getApplicationContext(), "Converting from MPG to KM/L", Toast.LENGTH_SHORT).show();
+                } else { //third case when both have input or just first field
+
+                    try {
+                        conv_value = Double.parseDouble(kml.getText().toString());
+                    } catch (NumberFormatException e) {
+                        conv_value = 0.0;
+                    }
+                    conv_value *= 2.35;
+                    result = String.format("%.2f",conv_value);
+                    mpg.setText(result);
+                    Toast.makeText(getApplicationContext(), "Converting from KM/L to MPG", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,6 +111,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
